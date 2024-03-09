f02da34a5cc20f966857c1ae883e01e33dfcb612
mohalearning <mohalearning01@gmail.com>
2024-03-09 21:29:11 +0100

    Feat: Finish the simple project but still needs improvment

--- index.js
+++ index.js
@@ -16,10 +16,14 @@ function multiply(number1, number2) {
 }
 
 function divide(number1, number2) {
+    if (number2 == "0")
+        return "Nan";
     return (number1 / parseFloat(number2));
 }
 
 function modulo(number1, number2) {
+    if (number2 == "0")
+        return "Nan";
     return (number1 / parseFloat(number2));
 }
 
@@ -43,10 +47,16 @@ function handleOperation(choosedOperation) {
         if (number2 === "")
             number2 = number1;
         currentResult = operate(parseInt(number1), parseInt(number2), operation);
+        if (currentResult.toString().length > 9)
+            currentResult = currentResult.toString().substring(0, 9);
         number1 = currentResult;
         number2 = "";
     }
     operation = choosedOperation;
+}
+
+function fixPrecision(result) {
+    return currentResult.toString().length > 9 ? currentResult.toString().substring(0, 9) : currentResult;
 }
 
 function resetVals() {
@@ -70,6 +80,7 @@ function checkButton(clickedButton) {
     if (clickedButton == "=") {
         if (operation !== "" && number1 !== "" && number2 !== "") {
             currentResult = operate(parseInt(number1), parseInt(number2), operation);
+            currentResult = fixPrecision(currentResult);
             resetVals();
         }
     }
@@ -79,25 +90,22 @@ function checkButton(clickedButton) {
             number2 = number2 === "0" ? number2 = "" : number2;
             number2 = number2.concat(clickedButton);
             currentResult = number2;
+            currentResult = fixPrecision(currentResult);
         }
         else {
             number1 = number1 === "0" ? number1 = "" : number1;
             number1 = number1.concat(clickedButton);
             currentResult = number1;
+            currentResult = fixPrecision(currentResult);
         }
     }
 
     if (clickedButton == "%") {
         currentResult = operate(number1, 10, "%");
         number1 = currentResult;
+        currentResult = fixPrecision(currentResult);
         number2 = "";
     }
-
-    // if (clickedButton == "+/-") {
-    //     if (number1 && number1[0] == '-')
-    //         number1 = number1.substring(1);
-    //     number1 = "-" + number1;
-    // }
 }
 
 function updateResult(_content) {
